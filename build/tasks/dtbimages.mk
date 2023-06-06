e405a68cab7ef6c6efe610bde2883012a1a4392d
Mao Han <han_mao@linux.alibaba.com>
2023-06-06 13:47:57 +0800

    Add support for BeagleV Ahead
    Change-Id: I8bfa7cdb28d3f60a9da23a802ecfc461d25e1e4d

--- build/tasks/dtbimages.mk
+++ build/tasks/dtbimages.mk
@@ -5,7 +5,7 @@
 # dtbo.img, and the resulting $(PRODUCT_OUT)/dtbo.img will be created with
 # Android build system, by exploiting BOARD_PREBUILT_DTBOIMAGE variable.
 
-ifneq ($(filter evb_light lichee_pi_4a light_b , $(TARGET_DEVICE)),)
+ifneq ($(filter beaglev_ahead evb_light lichee_pi_4a light_b , $(TARGET_DEVICE)),)
 
 PRODUCT_OUT ?= out/target/product/evb_light
 KERNEL_OUT ?= $(PRODUCT_OUT)/obj/KERNEL_OBJ
