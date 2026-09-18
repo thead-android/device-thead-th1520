5282b8d690347e58ffe2e1712f5fcfa5c929b373
LoveSy <shana@zju.edu.cn>
2026-09-18 09:41:23 +0000

    lpi4a: use Android 17 AIDL Wi-Fi and board overlay
    Replace the obsolete HIDL Wi-Fi services with Android 17's AIDL service and
    mainline supplicant.  Add the LPi4A resource overlay used by the validated
    AIC8800D configuration; explicit WPA3 remains available while automatic SAE
    upgrade is disabled for transition BSS compatibility.
    
    Signed-off-by: LoveSy <shana@zju.edu.cn>
    ---
    
    Archived input SHA256: f0faa36c6ee58e335cf1457502666508b02d81878deef9049c215f11d579c35e
    
    Signed-off-by: LoveSy <shana@zju.edu.cn>

--- subdevs/connectivity/connectivity.mk
+++ subdevs/connectivity/connectivity.mk
@@ -1,16 +1,9 @@
-# Wifi
+# Wi-Fi framework-facing AIDL HAL and nl80211 daemons.
 PRODUCT_PACKAGES += \
-	android.hardware.wifi@1.0-service
-
-# Add wifi-related packages
-PRODUCT_PACKAGES += \
+    android.hardware.wifi-service \
     wpa_supplicant \
     hostapd \
-    wpa_supplicant.conf \
+    Lpi4aWifiOverlay \
+    thead_wpa_supplicant.conf \
     hostapd.conf \
-    p2p_supplicant.conf \
-    android.hardware.wifi.supplicant@1.3-service \
-    android.hardware.wifi@1.0-service \
-    android.hardware.wifi@1.0-service-lib \
-    android.hardware.wifi.hostapd@1.2-service
-
+    p2p_supplicant.conf
